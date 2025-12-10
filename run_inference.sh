6ab15969c5e72e8baf0e8b2087e2789bd92ca80b
yuapi <phk2225@gmail.com>
2025-12-10 17:39:05 +0900

    [FEAT] 추론 스크립트 개선

--- run_inference.sh
+++ run_inference.sh
@@ -3,8 +3,12 @@ set -e
 
 # === [경로 설정] ===
 TEST_DATA_PATH="./data/test_dataset"
-MODEL_NAME="NLP-07-ODQA/roberta-large-tapt-n8-hybrid"
 SUBMISSION_DIR="./outputs/submission_tapt_roberta_2025"
+
+# 모델 설정
+READER_MODEL_NAME="NLP-07-ODQA/roberta-large-tapt-n8-hybrid"
+DENSE_MODEL_NAME="BAAI/bge-m3"
+RERANK_MODEL_NAME="BAAI/bge-reranker-v2-m3"
 
 
 echo ""
@@ -14,8 +18,10 @@ echo "----------------------------------------------------------------"
 
 python inference_roberta.py \
   --output_dir ${SUBMISSION_DIR} \
-  --model_name_or_path ${MODEL_NAME} \
   --dataset_name ${TEST_DATA_PATH} \
+  --model_name_or_path ${READER_MODEL_NAME} \
+  --dense_model_name ${DENSE_MODEL_NAME} \
+  --rerank_model_path ${RERANK_MODEL_NAME} \
   --do_predict
 
 echo "✅ TAPT Pipeline Finished!"
